1369c2b5bbf0c64a76e145b44c70c8281b137d0f
aharis <hare.arapovic@gmail.com>
2021-03-10 20:28:20 +0100

    dodao index.js sa komentarima

---
+++ ex02/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+    //Only change code below this line
+
+    //This is a single line JavaScript comment
+
+    /* document.write("This is multi-line comment!");
+     document.write("This is multi-line comment!"); 
+     document.write("This is multi-line comment!");
+     document.write("This is multi-line comment!");
+     document.write("This is multi-line comment!");
+    */
+
+
+    //Only change code above this line
+}
+
+    console.log(main());
+
+    module.exports = main;

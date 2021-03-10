7d1b104ad585b48902116693a81d428d9c2cc07b
aharis <hare.arapovic@gmail.com>
2021-03-10 21:49:43 +0100

    dodao camelCase variable sa vrijednostima

---
+++ ex07/src/index.js
@@ -0,0 +1,23 @@
+function main (){
+// Only change code below this line
+
+    // Variable declaration
+    var stadlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    // Variable assigment
+    stadlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+// Only change code above this line
+    
+    return {
+        stadlyCapVar,
+        properCamelCase,
+        titleCaseOver 
+    }
+}
+    console.log(main());
+    mudle.exports = main;
+
